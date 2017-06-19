ddd2bf12834b533b9a56a7f8480cdd7b17043164
dev-harsh1998 <harshitjain6751@gmail.com>
2017-06-19 12:18:19 +0530

    msm8916-common: force standard google gcc for arm platforms
    * Lineage's default kernel  gcc gives static liblog race conditions sometimes, making the zygote crash and device go bootloop in some random cases

--- board/kernel.mk
+++ board/kernel.mk
@@ -7,6 +7,12 @@ BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
+# Use Google GCC for arm platforms 
+ifeq ($(FORCE_32_BIT),true)
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
+endif
+
 ENABLE_CPUSETS := true
 
 TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8916
